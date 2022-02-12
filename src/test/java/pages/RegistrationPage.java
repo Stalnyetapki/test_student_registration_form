796b9df76ad8fc8811272f333980f077d1d98cbf
opachki <a.tupikov.miel@gmail.com>
2022-02-12 23:59:40 +0100

    added methods for entering data into the student registration form refactored StudentRegistrationFormWith PageObjects Tests test via new methods.

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -19,7 +19,19 @@ public class RegistrationPage {
             firstNameInput = $("#firstName"),
             lastNameInput = $("#lastName"),
             resultsForm = $(".table-responsive"),
-            mailInput = $("#userEmail");
+            mailInput = $("#userEmail"),
+            genderRadiobutton = $("#genterWrapper"),
+            phoneNumberInput = $("#userNumber"),
+            subjectsInput = $("#subjectsInput"),
+            hobbiesInput = $("#hobbiesWrapper"),
+            pictureUploadInput = $("#uploadPicture"),
+            addressInput = $("#currentAddress"),
+            stateInput = $("#react-select-3-input"),
+            cityInput = $("#react-select-4-input"),
+            submitButton = $("#submit"),
+            dateOfBirthInput = $("#dateOfBirthInput"),
+            popupTitle = $("#example-modal-sizes-title-lg");
+
 
     //actions
     public RegistrationPage openPage() {
@@ -47,9 +59,69 @@ public class RegistrationPage {
         return this;
     }
 
-    public void setBirthDate (String day, String month, String year) {
-        $("#dateOfBirthInput").click();
+    public RegistrationPage setGender(String gender) {
+        genderRadiobutton.$(byText(gender)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setPhoneNumber(String phoneNumber) {
+        phoneNumberInput.setValue(phoneNumber);
+
+        return this;
+    }
+
+    public RegistrationPage setSubject(String subject) {
+        subjectsInput.setValue(subject).pressEnter();
+
+        return this;
+    }
+
+    public RegistrationPage setHobby(String hobby) {
+        hobbiesInput.$(byText(hobby)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setPicture(String picture) {
+        pictureUploadInput.uploadFromClasspath(picture);
+
+        return this;
+    }
+
+    public RegistrationPage setAddress(String address) {
+        addressInput.setValue(address);
+
+        return this;
+    }
+
+    public RegistrationPage setState(String state) {
+        stateInput.setValue(state).pressEnter();
+
+        return this;
+    }
+
+    public RegistrationPage setCity(String city) {
+        cityInput.setValue(city).pressEnter();
+
+        return this;
+    }
+
+    public RegistrationPage checkPopupAppearance() {
+        popupTitle.shouldHave(text("Thanks for submitting the form"));
+
+        return this;
+    }
+
+    public void sendForm() {
+        submitButton.click();
+    }
+
+    public RegistrationPage setBirthDate (String day, String month, String year) {
+        dateOfBirthInput.click();
         calendarComponent.setDate(day, month, year);
+
+        return this;
 
     }
 
--- src/test/java/tests/StudentRegistrationFormWithPageObjectsTests.java
+++ src/test/java/tests/StudentRegistrationFormWithPageObjectsTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
+import java.sql.Time;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class StudentRegistrationFormWithPageObjectsTests {
@@ -17,13 +18,13 @@ public class StudentRegistrationFormWithPageObjectsTests {
     String lastName = "Marx";
     String mail = "karlmarx@test.ru";
     String gender = "Male";
-    String mobile = "9012709898";
+    String phoneNumber = "9012709898";
     String dayOfBirth = "25";
     String monthOfBirth = "October";
     String yearOfBirth = "1917";
-    String subjects = "English";
-    String hobbies = "Sports";
-    String picture = "kek.png";
+    String subject = "English";
+    String hobby = "Sports";
+    String nameOfPicture = "kek.png";
     String address = "Praha";
     String state = "NCR";
     String city = "Delhi";
@@ -42,29 +43,30 @@ public class StudentRegistrationFormWithPageObjectsTests {
                 .openPage()
                 .setFirstName(firstName)
                 .setLastName(lastName)
-                .setMail(mail);
-        $(byText(gender)).click();
-        $("#userNumber").setValue(mobile);
-        registrationPage.setBirthDate("25", "October", "1917");
-        $("#subjectsInput").setValue(subjects).pressEnter();
-        $(byText("Sports")).click();
-        $("#uploadPicture").uploadFromClasspath(picture);
-        $("#currentAddress").setValue(address);
-        $("#react-select-3-input").setValue(state).pressEnter();
-        $("#react-select-4-input").setValue(city).pressEnter();
-        $("#submit").click();
+                .setMail(mail)
+                .setGender(gender)
+                .setBirthDate("25", "October", "1917")
+                .setPhoneNumber(phoneNumber)
+                .setSubject(subject)
+                .setHobby(hobby)
+                .setPicture(nameOfPicture)
+                .setAddress(address)
+                .setState(state)
+                .setCity(city)
+                .sendForm();
+
 
         //compare input data with output data
-        $(".modal-header").shouldHave(text("Thanks for submitting the form"));
         registrationPage
+                .checkPopupAppearance()
                 .checkForm("Student Name", firstName + " " + lastName)
                 .checkForm("Student Email", mail)
                 .checkForm("Gender", gender)
-                .checkForm("Mobile", mobile)
+                .checkForm("Mobile", phoneNumber)
                 .checkForm("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
-                .checkForm("Subjects", subjects)
-                .checkForm("Hobbies", hobbies)
-                .checkForm("Picture", picture)
+                .checkForm("Subjects", subject)
+                .checkForm("Hobbies", hobby)
+                .checkForm("Picture", nameOfPicture)
                 .checkForm("Address", address)
                 .checkForm("State and City", state + " " + city);
 
