d4428c5df3ecaefbe8a099b11f5bc5e2d033a4f9
opachki <a.tupikov.miel@gmail.com>
2022-02-13 17:30:10 +0100

    moved test data to new class TestData

--- src/test/java/tests/StudentRegistrationFormWithPageObjectsTests.java
+++ src/test/java/tests/StudentRegistrationFormWithPageObjectsTests.java
@@ -4,31 +4,18 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
-
 import java.sql.Time;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static tests.TestData.*;
+
+import tests.TestData.*;
 
 
 public class StudentRegistrationFormWithPageObjectsTests extends TestBase{
 
     RegistrationPage registrationPage = new RegistrationPage();
-    String firstName = "Karl";
-    String lastName = "Marx";
-    String mail = "karlmarx@test.ru";
-    String gender = "Male";
-    String phoneNumber = "9012709898";
-    String dayOfBirth = "25";
-    String monthOfBirth = "October";
-    String yearOfBirth = "1917";
-    String subject = "English";
-    String hobby = "Sports";
-    String nameOfPicture = "kek.png";
-    String address = "Praha";
-    String state = "NCR";
-    String city = "Delhi";
 
     @Test
     void successFillTest() {
---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,19 @@
+package tests;
+
+public class TestData {
+
+    public static String firstName = "Karl",
+            lastName = "Marx",
+            mail = "karlmarx@test.ru",
+            gender = "Male",
+            phoneNumber = "9012709898",
+            dayOfBirth = "25",
+            monthOfBirth = "October",
+            yearOfBirth = "1917",
+            subject = "English",
+            hobby = "Sports",
+            nameOfPicture = "kek.png",
+            address = "Praha",
+            state = "NCR",
+            city = "Delhi";
+}
