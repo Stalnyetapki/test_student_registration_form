cece9f8f799ccf0e88b07a53372dd69bfe920fb7
Aleksei Tupikov <a.tupikov.miel@gmail.com>
2023-05-02 21:56:07 +0300

    added owner library

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,16 +2,24 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.AuthConfig;
+import config.TestConfig;
 import helpers.Attach;
 import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.Map;
+
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
+
+    static TestConfig testConfig = ConfigFactory.create(TestConfig.class, System.getProperties());
+    static AuthConfig authConfig = ConfigFactory.create(AuthConfig.class, System.getProperties());
 
     @BeforeAll
     @Step("Setting configuration")
@@ -19,19 +27,18 @@ public class TestBase {
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        String userLogin = System.getProperty("userLogin", UserCredential.userLogin);
-        String userPassword = System.getProperty("userPassword", UserCredential.userPassword);
-
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.remote = "https://" + userLogin + ":" + userPassword + "@"
-                + System.getProperty("remoteBrowser", "selenoid.autotests.cloud/wd/hub");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = testConfig.getBaseurl();
+        Configuration.browserSize = testConfig.getBrowserSize();
+        Configuration.browser = testConfig.getBrowserName();
+        if (testConfig.isRemote()) {
+            Configuration.remote = "https://" + authConfig.getUserLogin() + ":" + authConfig.getUserPassword() + "@"
+                    + testConfig.getRemoteUrl();
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true));
+            Configuration.browserCapabilities = capabilities;
+        }
     }
 
     @AfterEach
@@ -42,6 +49,4 @@ public class TestBase {
         Attach.addVideo();
         closeWebDriver();
     }
-
-
 }
